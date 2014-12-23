e0c5c8d8ddf4ad04680fa95a6d3fb2f3450b566f
Roman <mr_holmes@list.ru>
2014-12-23 19:08:46 +0300

    8

--- startmenu.pas
+++ startmenu.pas
@@ -12,6 +12,7 @@ type
     FControl: boolean;
     FStartMenuWnd: HWND;
     FDesktopUserPictureWnd: HWND;
+    FDesktopSpecialFoldersWnd: HWND;
   public
     constructor Create;
     procedure Show(site: TBaseSite; host_wnd: cardinal; baseRect, monitorRect: windows.TRect);
@@ -38,6 +39,7 @@ begin
   FStartMenuWnd := findwindow('ImmersiveLauncher', nil);
   if FStartMenuWnd = 0 then FStartMenuWnd := findwindow('DV2ControlHost', nil);
   FDesktopUserPictureWnd := findwindow('Desktop User Picture', nil);
+  //FDesktopSpecialFoldersWnd := findwindowex(FStartMenuWnd, 0, 'DesktopSpecialFolders', nil);
   GetWindowRect(FStartMenuWnd, @wRect);
   GetWindowRect(FDesktopUserPictureWnd, @picRect);
   FxPic := (wRect.Right - wRect.Left) * 3 div 4 - (picRect.Right - picRect.Left);
@@ -100,6 +102,7 @@ begin
         SetWindowPos(FStartMenuWnd, 0, Fx, Fy, 0, 0, SWP_NOSIZE + SWP_NOZORDER);
         SetWindowPos(FDesktopUserPictureWnd, 0, Fx + FxPic, Fy + FyPic, 0, 0, SWP_NOSIZE + SWP_NOZORDER);
       end;
+      //PostMessage(FDesktopSpecialFoldersWnd, WM_ERASEBKGND, 0, 0);
     end
     else FControl := false;
   end;
